8626d4f1def9691f8d452839ed996884c162d769
liuther9 <zd@akhter.studio>
2021-12-11 16:44:40 +0600

    font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,20 +5,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* <link
-            rel="preload"
-            href="/fonts/Klee_One/KleeOne-Regular.woff2"
-            as="font"
-            crossOrigin=""
-            type="font/woff2"
-          />
-          <link
-            rel="preload"
-            href="/fonts/Klee_One/KleeOne-SemiBold.woff2"
-            as="font"
-            crossOrigin=""
-            type="font/woff2"
-          /> */}
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
           <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;600&display=swap" rel="stylesheet"/>
