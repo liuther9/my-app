6bffe2ff5e286582f1bc2ba87a55c7edc1ccbe4a
liuther9 <zd@akhter.studio>
2021-12-12 13:43:54 +0600

    burger menu

--- components/Layout/Header/Header.tsx
+++ components/Layout/Header/Header.tsx
@@ -4,6 +4,7 @@ import s from './header.module.scss'
 import { useEffect, useState } from 'react'
 
 const Header:React.FC = () => {
+	const [menuActive, setMenuActive] = useState(false)
   const [small, setSmall] = useState(false);
 	const router = useRouter()
 	
@@ -16,9 +17,18 @@ const Header:React.FC = () => {
 		}
   }, []);
 
-	return <header className={!small ? s.header : s.header + ' ' + s.header_small}>
+	return <header className={!small ? s.header : `${s.header} ${s.header_small}`}>
 		<h1 onClick={() => router.push('/')}>NOOTS</h1>
-		<div className={s.links}>
+		<div className={!menuActive ? s.burger_btn : `${s.burger_btn} ${s.active}`} onClick={() => setMenuActive(!menuActive)}>
+			<i />
+		</div>
+		<div className={!menuActive ? s.links : `${s.links} ${s.show}`}>
+			<div className={s.burger_btn_dropdown}>
+				<div className={!menuActive ? s.burger_btn : `${s.burger_btn} ${s.active}`} onClick={() => setMenuActive(!menuActive)}>
+					<i />
+				</div>
+			</div>
+			<Link href='/'>Меню</Link>
 			<Link href='/about'>О нас</Link>
 			<Link href='/contacts'>Контакты</Link>
 		</div>
