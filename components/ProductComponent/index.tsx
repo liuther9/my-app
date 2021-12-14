a7e68b3a7ed5e7066b45f0e4036881a09dc412fa
liuther9 <zd@akhter.studio>
2021-12-14 16:22:14 +0600

    photos

--- components/ProductComponent/index.tsx
+++ components/ProductComponent/index.tsx
@@ -16,12 +16,14 @@ const ProductComponent: React.FC<Props> = ({product}) => {
 	const [showImg, setShowImg] = useState(false)
 
 	return <div className={styles.product}>
-		<div className={styles.name} onMouseEnter={() => setShowImg(true)} onMouseLeave={() => setShowImg(false)}>{name}</div>
+		<div className={styles.button_container}>
+			<button className={styles.name} onClick={() => setShowImg(!showImg)} onBlur={() => setShowImg(false)}>{name}</button>
+			<div className={!showImg ? styles.modal_img : `${styles.modal_img} ${styles.modal_show}`}>
+				{image && <Image alt='' src={image} width={300} height={200} objectFit='cover' />}
+			</div>
+		</div>
 		<div className={styles.description}>Здесь будет описание данного продукта</div>
 		<div className={styles.price}>{price} тенге</div>
-		<div className={!showImg ? styles.modal_img : `${styles.modal_img} ${styles.modal_show}`}>
-			{image && <Image alt='' src={image} width={300} height={200} objectFit='cover' />}
-		</div>
 	</div>
 }
 
