e2f0b6f336a177395f3f7c60f968efb459920e39
liuther9 <zd@akhter.studio>
2022-03-17 23:42:35 +0600

    bot

--- next.config.js
+++ next.config.js
@@ -1,42 +1,42 @@
 /** @type {import('next').NextConfig} */
-const { Markup, Telegraf } = require('telegraf')
-let token = process.env.NEXT_PUBLIC_BOT_TOKEN
+// const { Markup, Telegraf } = require('telegraf')
+// let token = process.env.NEXT_PUBLIC_BOT_TOKEN
 
-const bot = new Telegraf(token);
+// const bot = new Telegraf(token);
 
-bot.start((ctx) => {
-  ctx.reply('Hello ' + ctx.from.first_name + '!');
-});
+// bot.start((ctx) => {
+//   ctx.reply('Hello ' + ctx.from.first_name + '!');
+// });
 
-bot.help((ctx) => {
-  ctx.reply('Send /start to receive a greeting');
-  ctx.reply('Send /keyboard to receive a message with a keyboard');
-  ctx.reply('Send /quit to stop the bot');
-});
+// bot.help((ctx) => {
+//   ctx.reply('Send /start to receive a greeting');
+//   ctx.reply('Send /keyboard to receive a message with a keyboard');
+//   ctx.reply('Send /quit to stop the bot');
+// });
 
-bot.command('quit', (ctx) => {
-  // Explicit usage
-  ctx.telegram.leaveChat(ctx.message.chat.id);
+// bot.command('quit', (ctx) => {
+//   // Explicit usage
+//   ctx.telegram.leaveChat(ctx.message.chat.id);
 
-  // Context shortcut
-  ctx.leaveChat();
-});
+//   // Context shortcut
+//   ctx.leaveChat();
+// });
 
-bot.command('keyboard', (ctx) => {
-  ctx.reply(
-    'Keyboard',
-    Markup.inlineKeyboard([
-      Markup.button.callback('First option', 'first'),
-      Markup.button.callback('Second option', 'second'),
-    ])
-  );
-});
+// bot.command('keyboard', (ctx) => {
+//   ctx.reply(
+//     'Keyboard',
+//     Markup.inlineKeyboard([
+//       Markup.button.callback('First option', 'first'),
+//       Markup.button.callback('Second option', 'second'),
+//     ])
+//   );
+// });
 
-bot.launch();
+// bot.launch();
 
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+// // Enable graceful stop
+// process.once('SIGINT', () => bot.stop('SIGINT'));
+// process.once('SIGTERM', () => bot.stop('SIGTERM'));
 
 module.exports = {
   reactStrictMode: true,
