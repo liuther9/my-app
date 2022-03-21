bee0000eaa0c482e4f17abc3bcc3b66db2bf8731
liuther9 <zd@akhter.studio>
2022-03-21 15:11:31 +0600

    order info telegram

--- pages/api/order.ts
+++ pages/api/order.ts
@@ -3,9 +3,12 @@ import { supabase } from "../../utils/supabaseClient";
 
 export default async function handler(req:NextApiRequest, res:NextApiResponse) {
 	const { user_id, payment_type, address, order_list, total } = req.body
+	
+	//AUTHORIZATION
 	const JWT = req.headers.authorization
 	supabase.auth.setAuth(JWT ? JWT : '')
 
+	//ADD NEW ORDER
 	const {data, error} = await supabase.from('ORDERS').insert(
 		[
 			{
@@ -20,14 +23,20 @@ export default async function handler(req:NextApiRequest, res:NextApiResponse) {
 
 	if(error) return res.status(400).json({ error: error.message })
 
+	//SEND ORDER INFO TO TELEGRAM
 	const order = data && data[0]
+	const user_info = await supabase.from('USER_INFO').select('address, phone').eq('id', order.address).then(res => res.data)
+	const user_phone = user_info ? user_info[0].phone : {}
+	const user_address = user_info ? user_info[0].address : {}
+	const { building, street, appartment } = user_address
 
 	const sendMessage = `
 		${order.order_list?.map((item: { name: string; quantity: string | number; }) => {
 			return `Товар: ${item.name} ${item.quantity} шт.%0A`
 			})}
 		Общая сумма: ${order.total}%0A
-		Адрес: ${order.address}%0A
+		Адрес: ${street + ' ' + building + ', ' + (appartment && ('квартира ' + appartment))}%0A
+		Телефон: ${user_phone}%0A
 		Способ оплаты: ${order.payment_type}%0A
 		Статус оплаты:${order.payed?'Оплачено':'Не оплачено'}
 	`
