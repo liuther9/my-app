8b196f6cc9f5440f057f1511c20e4dcfba4910aa
liuther9 <zd@akhter.studio>
2022-03-22 17:36:43 +0600

    telegram auth

--- utils/checkTelegram.ts
+++ utils/checkTelegram.ts
@@ -4,7 +4,9 @@ import Hex from 'crypto-js/enc-hex'
 
 const checkTelegram = ({ auth_date, first_name, username, hash, id }: {id: any, auth_date: any, first_name: any, username: any, hash: any}) => {
 	const data_check_string = `auth_date=${auth_date}\nfirst_name=${first_name}\nid=${id}\nusername=${username}`
+	console.log(data_check_string)
 	const secret_key = sha256(process.env.NEXT_PUBLIC_BOT_TOKEN ? process.env.NEXT_PUBLIC_BOT_TOKEN : '')
+	console.log(process.env.NEXT_PUBLIC_BOT_TOKEN)
 	console.log(hmacSHA512(data_check_string, secret_key).toString(Hex))
 	if (hmacSHA512(data_check_string, secret_key).toString(Hex) == hash) {
 		console.log('LOOGGGGED IN')
