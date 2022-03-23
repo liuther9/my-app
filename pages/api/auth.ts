fd52eb857c50f36ad6173476eb3accaf0a964c04
liuther9 <zd@akhter.studio>
2022-03-23 14:14:29 +0600

    tg auth

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -7,36 +7,36 @@ import Hex from 'crypto-js/enc-hex'
 import { serialize } from 'cookie'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-	// if(req.method === 'POST') {
-	// 	const { auth_date, first_name, id, username, hash } = req.body
+	if(req.method === 'POST') {
+		const { auth_date, first_name, id, username, hash } = req.body
 
-	// 	// CHECK USER
-	// 	const data_check_string = `auth_date=${auth_date}\nfirst_name=${first_name}\nid=${id}\nusername=${username}`
-	// 	const secret_key = sha256(process.env.BOT_TOKEN ? process.env.BOT_TOKEN : '')
-	// 	if (hmacSHA256(data_check_string, secret_key).toString(Hex) == hash) {
-	// 		res.status(401)
-	// 		res.end('Data is NOT from Telegram')
-	// 	}
-	// 	const user = await supabase.from('USERS').select('*').eq('id', id)
+		// CHECK USER
+		const data_check_string = `auth_date=${auth_date}\nfirst_name=${first_name}\nid=${id}\nusername=${username}`
+		const secret_key = sha256(process.env.BOT_TOKEN ? process.env.BOT_TOKEN : '')
+		if (hmacSHA256(data_check_string, secret_key).toString(Hex) == hash) {
+			res.status(401)
+			res.end('Data is NOT from Telegram')
+		}
+		const user = await supabase.from('USERS').select('*').eq('id', id)
 		
-	// 	// UPDATE USER
-	// 	await supabase.from('USERS').upsert({
-	// 		id,
-	// 		username,
-	// 		first_name,
-	// 		auth_date
-	// 	}, { returning: 'minimal' })
+		// UPDATE USER
+		await supabase.from('USERS').upsert({
+			id,
+			username,
+			first_name,
+			auth_date
+		}, { returning: 'minimal' })
 
-	// 	// TOKEN SIGN
-	// 	if (!user) {
-	// 		const token = jwt.sign(id, process.env.JWT_SECRET ? process.env.JWT_SECRET : '', { expiresIn: 60*60*24 });
-	// 		res.status(200).setHeader('Set-Cookie', serialize('tgToken', token, { path: "/" }));
-	// 	}
-	// 	if ( (+(new Date()) - auth_date) > 60*60*24) {
-	// 		const token = jwt.sign(id, process.env.JWT_SECRET ? process.env.JWT_SECRET : '', { expiresIn: 60*60*24 });
-	// 		res.status(200).setHeader('Set-Cookie', serialize('tgToken', token, { path: "/" }));
-	// 	}
-	// }
+		// TOKEN SIGN
+		if (!user) {
+			const token = jwt.sign(id, process.env.JWT_SECRET ? process.env.JWT_SECRET : '', { expiresIn: 60*60*24 });
+			res.status(200).setHeader('Set-Cookie', serialize('tgToken', token, { path: "/" }));
+		}
+		if ( (+(new Date()) - auth_date) > 60*60*24) {
+			const token = jwt.sign(id, process.env.JWT_SECRET ? process.env.JWT_SECRET : '', { expiresIn: 60*60*24 });
+			res.status(200).setHeader('Set-Cookie', serialize('tgToken', token, { path: "/" }));
+		}
+	}
 	
-	supabase.auth.api.setAuthCookie(req, res)
+	// supabase.auth.api.setAuthCookie(req, res)
 }
